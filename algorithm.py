778cb2945b0c72281845d6a3f2e9b4b1721b7e57
Biraj ST <birajs.2002@gmail.com>
2024-01-23 22:10:02 +0545

    Algorithm Commit

--- algorithm.py
+++ algorithm.py
@@ -51,24 +51,3 @@ def recommend_post(user, post_list):
 
     return list(dict(sorted(posts.items(), key=lambda item: -item[1])).keys())
 
-
-# def recommend_post(user, post_list):
-#     posts = {}
-#     if not user:
-#         return post_list
-#     for post in post_list:
-#         point = 0
-#         for time_spent in user.time_spent:
-#             point += cosine_similarity(time_spent.post.content,  post.content) *time_spent.time
-#             point *= 0.5 if time_spent.post == post else 1.5
-#         for topic in user.topics.split(','):
-#             point += 40 * (topic in post.title)
-#             point += 6 * min(post.content.count(topic), 5)
-#         point += 60 * post.content_type == user.preferences
-#         point += 0.01 * min(len(post.content), 10_000)
-#         point += 5 * post.overall_rating * len(post.ratings)
-#         point += 0.02 * (post.date.day + post.date.month*1.2 + post.date.year * 1.5)
-#         posts[post] = point
-#     return list(dict(sorted(posts.items(), key=lambda item: -item[1])).keys())
-
-
